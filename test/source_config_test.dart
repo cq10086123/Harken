c2786da5bcd80a4008d39b9ff19862425d335b75
cq10086123 <105488660+cq10086123@users.noreply.github.com>
2026-09-18 14:30:11 +0800

    test: 补 source_caps.dart 导入（AudioSourceCaps 未定义）

--- test/source_config_test.dart
+++ test/source_config_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:feiniu_music/app/services/source/source_caps.dart';
 import 'package:feiniu_music/app/services/source/source_config.dart';
 import 'package:feiniu_music/app/services/source/source_kind.dart';
 
